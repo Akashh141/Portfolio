547542345837df9b105fc2e496518f06739556cf
Akashh001 <protongithub1@protonmail.com>
2024-03-24 14:02:00 +0530

    feat: add geometrical shapes

--- src/slices/Hero/index.tsx
+++ src/slices/Hero/index.tsx
@@ -4,6 +4,7 @@ import { Content, KeyTextField,  } from "@prismicio/client";
 import { SliceComponentProps} from "@prismicio/react";
 import gsap from 'gsap'
 import Bounded from "@/components/Bounded";
+import Shapes from "@/slices/Hero/Shapes"
 
 /**
  * Props for `Hero`.
@@ -77,7 +78,7 @@ const Hero = ({ slice }: HeroProps): JSX.Element => {
       ref={component}
     >
       <div className="grid min-h-[70vh] grid-cols-1 items-center md:grid-cols-2">
-        
+        <Shapes />
         <div className="col-start-1 md:row-start-1 " data-speed=".2">
           <h1
             className="mb-8 text-[clamp(3rem,20vmin,20rem)] font-extrabold leading-none tracking-tighter"
