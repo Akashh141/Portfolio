cff2e3f8eb9f86f14e8fc43a9396020e433a09de
Akashh001 <protongithub1@protonmail.com>
2024-03-30 08:57:38 +0530

    fea: contentbody

--- src/app/slice-simulator/page.tsx
+++ src/app/slice-simulator/page.tsx
@@ -1,20 +1,43 @@
-import {
-  SliceSimulator,
-  SliceSimulatorParams,
-  getSlices,
-} from "@slicemachine/adapter-next/simulator";
-import { SliceZone } from "@prismicio/react";
+import { Metadata } from "next";
+import { notFound } from "next/navigation";
 
-import { components } from "../../slices";
+import { createClient } from "@/prismicio";
 
-export default function SliceSimulatorPage({
-  searchParams,
-}: SliceSimulatorParams) {
-  const slices = getSlices(searchParams.state);
+import ContentBody from "@/components/ContentBody";
+import { formatDate } from "@/utils/formatDate";
 
-  return (
-    <SliceSimulator>
-      <SliceZone slices={slices} components={components} />
-    </SliceSimulator>
-  );
+type Params = { uid: string };
+
+export default async function Page({ params }: { params: Params }) {
+  const client = createClient();
+  const page = await client
+    .getByUID("project", params.uid)
+    .catch(() => notFound());
+
+  return <ContentBody page={page} />;
+}
+
+export async function generateMetadata({
+  params,
+}: {
+  params: Params;
+}): Promise<Metadata> {
+  const client = createClient();
+  const page = await client
+    .getByUID("project", params.uid)
+    .catch(() => notFound());
+
+  return {
+    title: page.data.title,
+    description: page.data.meta_description,
+  };
+}
+
+export async function generateStaticParams() {
+  const client = createClient();
+  const pages = await client.getAllByType("project");
+
+  return pages.map((page) => {
+    return { uid: page.uid };
+  });
 }
